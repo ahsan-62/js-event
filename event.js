10bbeee18aca7b847475fa6b964d772a62c7afb7
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-04-21 11:05:44 +0600

    onclick Event Handler

---
+++ event.js
@@ -0,0 +1,21 @@
+
+
+//Third Option for event handler onclick
+
+const ahsan=document.getElementById('green');
+ahsan.onclick=function greenColor(){
+
+    document.body.style.backgroundColor="green";
+
+}
+
+//Fourth Option for event handler onclick
+
+
+const jun=document.getElementById('blue');
+
+jun.onclick=blueColor;
+function blueColor(){
+
+    document.body.style.backgroundColor="blue";
+}
